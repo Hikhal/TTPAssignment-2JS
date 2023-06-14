7c4a6555c88ac1298d306360bec66be68aa40d37
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-13 20:29:33 -0400

    comments to myFilter.js have been uploaded

--- myFilter.js
+++ myFilter.js
@@ -1,5 +1,9 @@
-
-
+// The myFilter function is a custom implementation of the filter method provided in JavaScript.
+// It takes an array and a callback function as its parameters. 
+// The function iterates over each element in the array and applies the callback function to it.
+// If the callback function returns true for a particular element, that element is added to a new array.
+// After all elements have been processed, the new array is returned.
+// The original array is not modified by the function.
 
 function myFilter(array, callback){
     let result = []
