092cff86c8e02b02d7ed95da467d610bef73966b
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-13 20:15:02 -0400

    comments to myMap.js added

--- myMap.js
+++ myMap.js
@@ -1,4 +1,10 @@
-
+//
+// The function myMap takes an array and a callback function as its parameters and 
+// returns a new array containing the elements of the array modified by the callback function.
+// The new array is returned when all the elements have been processed.
+// A crucial aspect of the map function is that the original array is not altered. 
+// By applying the callback function to each element of the corresponding input array element,
+// the myMap function converts an input array into a new output array.
 
 function myMap(array, callback){
     let result = []
