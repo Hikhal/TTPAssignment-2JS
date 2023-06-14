c3a16e35813cafa03b6476f888609c3582444787
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-13 22:18:21 -0400

    comments added to grabValues

--- grabValues.js
+++ grabValues.js
@@ -1,4 +1,8 @@
-
+// The grabValues function is a custom implementation of retrieving all the values of an object in JavaScript without using the native Object.values method.
+// It takes an object as a parameter and uses a for...in loop to iterate over the object's properties.
+// The function checks if each key belongs to the object itself (not inherited from the prototype chain) using hasOwnProperty.
+// If the key belongs to the object, it accesses the corresponding value using the key and adds it to the values array.
+// The values array is then returned, containing all the values of the object.
 
 function grabKeys(obj) {
     const keys = [];
